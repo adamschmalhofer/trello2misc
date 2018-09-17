850e761a1b960306bab4202c7dbbbbaf67d68705
Adam Schmalhofer <Adam.Schmalhofer@gmx.de>
2018-09-17 19:00:41 +0200

    Made todotxt.py pep8 compliant

--- todotxt.py
+++ todotxt.py
@@ -2,10 +2,12 @@
 Contains the TodotxtTask class plus methods related to todo.txt.
 '''
 
-import re, datetime
+import re
+import datetime
 import utils
 
-# A todo.txt task contains the text entry, the priority, the project, 
+
+# A todo.txt task contains the text entry, the priority, the project,
 # the context, and the due date of the task.
 class TodotxtTask:
 
@@ -13,18 +15,18 @@ class TodotxtTask:
     def __init__(self, entry, priority, project, context, due):
         self.entry = re.sub("^\\(\d+\\)\s+", "", entry).strip()
         self.priority = priority.strip()
-        self.project = project.title().replace(" ","").strip()
-        self.context = context.replace(" ","").lower().strip()
+        self.project = project.title().replace(" ", "").strip()
+        self.context = context.replace(" ", "").lower().strip()
         self.due = due
         if len(self.due) > 0:
             try:
-                if "-" in self.due:
-                    self.due = datetime.datetime.strptime(self.due, "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%d")
-                else:
-                    self.due = datetime.datetime.strptime(self.due, "%dT%H:%M:%S.%fZ").strftime("%d")
+                input_format = ("%Y-%m-%dT%H:%M:%S.%fZ"
+                                if "-" in self.due else "%dT%H:%M:%S.%fZ")
+                self.due = datetime.datetime.strptime(self.due, input_format
+                                                      ).strftime("%d")
             except ValueError:
                 self.due = ""
-    
+
     # The one line string representation of a task.
     def __repr__(self):
         string = ""
@@ -38,7 +40,7 @@ class TodotxtTask:
         if len(self.due) > 0:
             string += " due:%s" % (self.due)
         return string
-    
+
     # Two tasks are equal if the text entry is the same.
     # This enables other things, such as priorities and due dates,
     # to be updated.
@@ -59,6 +61,7 @@ def read_todotxtfile():
         tasks.append(task)
     return tasks
 
+
 # Returns a task object from a one line string representation.
 def parse_todotxtline(line):
     entry = ""
@@ -68,7 +71,8 @@ def parse_todotxtline(line):
     due = ""
     tokens = re.split(r'\s+', line)
     index = 0
-    if len(tokens[index]) == 3 and tokens[index].startswith('(') and tokens[index].endswith(')'):
+    if (len(tokens[index]) == 3
+            and tokens[index].startswith('(') and tokens[index].endswith(')')):
         priority = tokens[index][1:-1]
         index = 1
     for i in range(index, len(tokens)):
@@ -85,6 +89,7 @@ def parse_todotxtline(line):
     task = TodotxtTask(entry, priority, project, context, due)
     return task
 
+
 # Writes a list of tasks to the todo.txt file.
 def write_tasks(tasks):
     config = utils.readconfig("trello2misc.ini")
@@ -93,4 +98,3 @@ def write_tasks(tasks):
     for task in tasks:
         theFile.write(repr(task) + "\n")
     theFile.close()
-
